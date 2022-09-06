9465c07bacfd961e76c184d3fb78946e3569ea28
Ilja Livenson <livenson@users.noreply.github.com>
2022-09-06 22:06:27 +0300

    Add basic logging

--- waldur_rt_sync/main.py
+++ waldur_rt_sync/main.py
@@ -1,8 +1,19 @@
 from time import sleep
+import logging
 
 from tasks import pull_issues
 
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(filename)s:%(lineno)d %(levelname)s - %(message)s",
+)
+
+logger = logging.getLogger(__name__)
+
+
 if __name__ == "__main__":
     while True:
+        logger.info('Looking for new tasks in RT...')
         pull_issues()
         sleep(float(60 * 10))
